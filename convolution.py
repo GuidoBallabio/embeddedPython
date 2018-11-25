d1b41d1adefde1eb3fa1028c1052692ccb2f3051
Daniella1 <au513437@uni.au.dk>
2018-11-25 17:44:30 +0100

    Added convolution separable

--- convolution.py
+++ convolution.py
@@ -24,3 +24,36 @@ def convolution(img, kernel):
                     s = s + (img[row_offset, col_offset]) * kernel[k_row,k_col]
             out[row,col] = s
     return out
+
+
+def convolution_separable(data, kernel):
+    U,S,V = np.linalg.svd(kernel)
+    s = S[0] # scaling factor
+    v = U[:,0]
+    h = V.T[:,0]
+
+    #a = np.convolve(data,v,mode="same")
+    #b = np.convolve(a,h,mode="same")
+
+    nK = kernel.shape[1] # assume square matrix
+    offset = int((nK-1)/2)
+    nData = data.shape[0]
+
+    outA = np.zeros(nData)
+    outB = np.zeros(nData)
+
+    for i in range(offset,nData-offset):
+        out_sum = 0
+        for k in range(0,nK):
+            row_offset = i - offset + k
+            out_sum = out_sum + (data[row_offset]) * v[k]
+        outA[i] = out_sum
+
+    for i in range(offset,nData-offset):
+        out_sum = 0
+        for k in range(0,nK):
+            row_offset = i - offset + k
+            out_sum = out_sum + (outA[row_offset]) * h[k]
+        outB[i] = out_sum
+
+    return outB
